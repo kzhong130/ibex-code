66fda6e94aed24a5447ec3acb6dadc2c4394fe73
kezhon0 <kezhon0@localhost>
2022-11-28 19:39:28 -0700

    rename to aggregator

---
+++ aggregator/build/CMakeFiles/aggregator_micro_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/kezhon0/ibex-code/seal/aggregator_micro_test.cpp" "/users/kezhon0/ibex-code/seal/build/CMakeFiles/aggregator_micro_test.dir/aggregator_micro_test.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/usr/local/include/SEAL-4.0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
